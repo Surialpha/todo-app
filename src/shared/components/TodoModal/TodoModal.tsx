01cdfd356235294396e2d5cf888e9623b2a6c5b5
Surialpha <sgp_96@live.com>
2022-06-21 09:33:49 -0500

    Adding close on click out of the modal

--- src/shared/components/TodoModal/TodoModal.tsx
+++ src/shared/components/TodoModal/TodoModal.tsx
@@ -1,3 +1,4 @@
+import { MouseEventHandler } from "react";
 import ReactDOM from "react-dom";
 import { useTodoContext } from "../../hooks/UseTodoContext";
 import { PropModel } from "../../models/PropChildrenModel";
@@ -6,11 +7,19 @@ import "./TodoModal.scss";
 function TodoModal(props: PropModel) {
   const portalDiv = document.getElementById("modal")!;
   const { showModal, setShowModal } = useTodoContext();
+
+  const closeOnClickOut = (e: MouseEventHandler<HTMLElement> | any) => {
+    if (e.target.id === "modal-container") setShowModal(!showModal);
+  };
+
   return ReactDOM.createPortal(
     showModal && (
-      <section id="modal-container">
+      <section id="modal-container" onClick={closeOnClickOut}>
         <div className="modal-body">
-          <span className="close-modal" onClick={() => setShowModal(!showModal)}>
+          <span
+            className="close-modal"
+            onClick={() => setShowModal(!showModal)}
+          >
             &#10006;
           </span>
           {props.children}
